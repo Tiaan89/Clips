57ba21baab062104099fd9ccc85b3b39a64aaa71
Dirk Robinson <tiaan.robinson@gmail.com>
2022-04-27 10:25:39 +0200

    Form Control, Binding and Validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,7 +8,14 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required
+      ]),
 
+      email: new FormControl(''),
+      age: new FormControl(''),
+      password: new FormControl(''),
+      confirm_password : new FormControl(''),
+      phoneNumber: new FormControl('')
   })
-
 }
