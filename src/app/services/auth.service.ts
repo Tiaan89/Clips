04994a23adbadfccf3227cfc34d454f5e271b1db
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-01 15:28:02 +0200

    Added Collection Types

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,29 +1,38 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private usersCollection: AngularFirestoreCollection<IUser>
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
-  ) { }
+  ) {
+    this.usersCollection = db.collection('users')
+  }
 
 //userData data type is 'any' - this is a problem (create an interface and inport it)
-public async createUser(userData: IUser){
-  const userCred = await this.auth.createUserWithEmailAndPassword(
+public async createUser(userData: IUser)
+  {
+    if(!userData.password){
+      throw new Error("Password not provided!")
+    }
+
+    const userCred = await this.auth.createUserWithEmailAndPassword(
     userData.email, userData.password
   )
-
-  await this.db.collection('users').add({
-    name: userData.name,
-    email: userData.email,
-    age: userData.age,
-    phoneNumber: userData.phoneNumber
-  })
-}
+//to limit the data - add IUser and make the password optional in the model and add conditional statement in the createUser method.
+  await this.usersCollection.add(
+    {
+      name: userData.name,
+      email: userData.email,
+      age: userData.age,
+      phoneNumber: userData.phoneNumber
+    })
+  }
 }
