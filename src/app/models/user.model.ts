22074d905a80eed36d583c74797207a90f6af1d2
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-01 15:06:07 +0200

    Refactor to a Service (Interface & Class)

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,16 @@
+export default interface IUser {
+  email: string,
+  password: string,
+  age: number,
+  name: string,
+  phoneNumber: string
+}
+
+//Creating a model with a class.
+// export default class IUser {
+//   email?: string,
+//   password?: string,
+//   age?: number,
+//   name?: string,
+//   phoneNumber?: string
+// }
