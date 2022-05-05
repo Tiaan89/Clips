fbfac5685131c861ecc736c251bbb936ade77cb3
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-05 14:44:51 +0200

    Querying the Database

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-manage',
@@ -11,13 +12,15 @@ export class ManageComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private clipService: ClipService
     ) { }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params.sort === '2' ? params.sort : '1'
     })
+    this.clipService.getUserClips().subscribe(console.log)
   }
 
   sort(event: Event) {
