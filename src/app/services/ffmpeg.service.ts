a32b992611156f1769c8b06e9de469090310ad39
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-09 15:10:05 +0200

    Generating a Screenshot

---
+++ src/app/services/ffmpeg.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FfmpegService {
+  isReady = false
+  private ffmpeg
+
+  constructor() {
+    this.ffmpeg = createFFmpeg({ log: true })
+   }
+
+   async init() {
+     if(this.isReady) {
+       return
+     }
+
+     await this.ffmpeg.load()
+
+     this.isReady = true
+   }
+
+   async getScreenshots(file: File) {
+    const data = await fetchFile(file)
+
+    //File System
+    this.ffmpeg.FS('writeFile', file.name, data)
+
+    await this.ffmpeg.run(
+      //Input
+      '-i', file.name,
+      //Output Options
+      '-ss', '00:00:02',
+      '-frames:v', '1',
+      '-filter:v', 'scale=510:-1',
+      //Output
+      'output_01.png'
+    )
+   }
+}
