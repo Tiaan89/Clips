6addbbbe9ce9c9080179ad1cdb0ffdae861eac92
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-09 15:17:32 +0200

    Generating multiple screenshots

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -28,15 +28,24 @@ export class FfmpegService {
     //File System
     this.ffmpeg.FS('writeFile', file.name, data)
 
+    const seconds = [1,2,3]
+    const commands: string[] = []
+
+    seconds.forEach(second => {
+      commands.push(
+        //Input
+        '-i', file.name,
+        //Output Options
+        '-ss', `00:00:0${second}`,
+        '-frames:v', '1',
+        '-filter:v', 'scale=510:-1',
+        //Output
+        `output_0${second}.png`
+      )
+    })
+
     await this.ffmpeg.run(
-      //Input
-      '-i', file.name,
-      //Output Options
-      '-ss', '00:00:02',
-      '-frames:v', '1',
-      '-filter:v', 'scale=510:-1',
-      //Output
-      'output_01.png'
+      ...commands
     )
    }
 }
