4c92be6903ad47ba20729fa6484e02940badf50d
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-11 12:12:51 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDPHYcLMOwLQcepTaqGoSrOP9EZJfqKR3M",
+    authDomain: "clips-40d04.firebaseapp.com",
+    projectId: "clips-40d04",
+    storageBucket: "clips-40d04.appspot.com",
+    appId: "1:1042194936125:web:59f72fb5cf9afc9678d554"
+  }
 };
